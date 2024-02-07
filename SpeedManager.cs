c95b097deaf3d82f3902d5f33bce336f425b85f8
Nylux <nylux42@gmail.com>
2024-02-07 02:21:59 +0100

    Updated for latest MSL release

--- SpeedManager.cs
+++ SpeedManager.cs
@@ -10,12 +10,12 @@ public class SpeedManager : Mod
     public override string Name => "SpeedManager";
     public override string Author => "Nylux";
     public override string Description => "Change the game's speed at will.";
-    public override string Version => "1.1.0";
+    public override string Version => "1.1.1";
 
     public override void PatchMod()
     {
-        ModLoader.InsertGMLString(ModFiles.GetCode("speedF3.gml"), "gml_Object_o_player_KeyPress_114", 0);
-        ModLoader.InsertGMLString(ModFiles.GetCode("speedF4.gml"), "gml_Object_o_player_KeyPress_115", 0);
-        ModLoader.InsertGMLString(ModFiles.GetCode("speedAlarm7.gml"), "gml_Object_o_player_Alarm_7", 0);
+        Msl.InsertGMLString(ModFiles.GetCode("speedF3.gml"), "gml_Object_o_player_KeyPress_114", 0);
+        Msl.InsertGMLString(ModFiles.GetCode("speedF4.gml"), "gml_Object_o_player_KeyPress_115", 0);
+        Msl.InsertGMLString(ModFiles.GetCode("speedAlarm7.gml"), "gml_Object_o_player_Alarm_7", 0);
     }
 }
